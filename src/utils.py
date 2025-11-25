1a3a1d2563dfa152a1099e2adfcd7051b22f706b
Katherine Yan <katyan@mit.edu>
2025-11-25 03:37:19 +0000

    created save/load context stuff

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ from pydrake.all import (
     MultibodyPlant
 )
 
+from pydrake.systems.framework import Context
+
 def randomize_blocks(blocks : list[str], plant, plant_context):
 
     def sample_drop_point(inner_d = 0.2, outer_d = 0.8):
@@ -25,3 +27,20 @@ def randomize_blocks(blocks : list[str], plant, plant_context):
             body,
             RigidTransform(rotation, position)
         )
+
+def save_context(context : Context, filename : str):
+    time = context.get_time()
+    xc = context.get_continuous_state_vector().CopyToVector()
+    xd = context.get_discrete_state(0).CopyToVector()
+
+    np.savez(filename, time=time, xc=xc, xd=xd)
+
+def load_context(context : Context, filename : str):
+    with np.load(filename) as data:
+        time = data["time"]
+        xc = data["xc"]
+        xd = data["xd"]
+    
+    context.SetTime(time)
+    context.SetContinuousState(xc)
+    context.SetDiscreteState(0, xd)
